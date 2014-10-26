fa7df0c00d993dbe628bd3b456c075cfd914c884
Ben Gott <bengott@gmail.com>
2014-10-25 20:59:34 -0500

    Clean up, remove some logging

--- server/methods.js
+++ server/methods.js
@@ -3,23 +3,21 @@ Meteor.methods({
   chickenOut: function (driver) {
 
     check(driver, String);
-    console.log("chickenOut", driver);
 
     // Enforce a cutoff point for chickening out (2 timesteps before true midpoint)
     var distanceX = Timing.timestep.get() * DX;
     var cutoffX = ROAD_WIDTH/2 - CAR_WIDTH - DX*2;
     var carId;
-    
+
     if (distanceX < cutoffX) {
-      
+
       carId = null;
       if      (driver === Cars.findOne("A").driver) carId = "A";
       else if (driver === Cars.findOne("B").driver) carId = "B";
-      
-      console.log(driver, carId);
+
       if (carId) {
         Cars.update(carId, {$set: {chickenedOut: Date.now()}});
-        console.log(driver + " (car "+carId+") chickened out");
+        console.log(driver + " (car " + carId + ") chickened out");
       }
 
     } else {
